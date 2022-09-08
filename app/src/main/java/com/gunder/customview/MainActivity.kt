d4e2f79f4108ef9519b6382445000a3b1a6fdb85
itselaras <gunadermawan62@gmail.com>
2022-09-08 22:10:32 +0700

    create custom edit text

--- app/src/main/java/com/gunder/customview/MainActivity.kt
+++ app/src/main/java/com/gunder/customview/MainActivity.kt
@@ -1,11 +1,42 @@
 package com.gunder.customview
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var myButton: MyButton
+    private lateinit var myEditText: MyEditText
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        myButton = findViewById(R.id.myButton)
+        myEditText = findViewById(R.id.myEditText)
+        setMyButtonEnabled()
+
+        myEditText.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+
+            }
+
+            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+                setMyButtonEnabled()
+            }
+
+            override fun afterTextChanged(p0: Editable?) {
+                // do nothing
+            }
+
+        })
+        myButton.setOnClickListener {
+            Toast.makeText(this@MainActivity, myEditText.text, Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    private fun setMyButtonEnabled() {
+        val result = myEditText.text
+        myButton.isEnabled = result != null && result.toString().isNotEmpty()
     }
 }
---
+++ app/src/main/java/com/gunder/customview/MyEditText.kt
@@ -0,0 +1,114 @@
+package com.gunder.customview
+
+import android.content.Context
+import android.graphics.Canvas
+import android.graphics.drawable.Drawable
+import android.text.Editable
+import android.text.TextWatcher
+import android.util.AttributeSet
+import android.view.MotionEvent
+import android.view.View
+import androidx.appcompat.widget.AppCompatEditText
+import androidx.core.content.ContextCompat
+
+class MyEditText : AppCompatEditText, View.OnTouchListener {
+    private lateinit var clearBtnImage: Drawable
+
+    constructor(context: Context) : super(context) {
+        init()
+    }
+
+    constructor(context: Context, attrs: AttributeSet) : super(context, attrs) {
+        init()
+    }
+
+    constructor(context: Context, attrs: AttributeSet, defStyleAttr: Int) : super(context,
+        attrs,
+        defStyleAttr) {
+        init()
+    }
+
+    override fun onDraw(canvas: Canvas?) {
+        super.onDraw(canvas)
+        hint = "masukan nama Anda"
+        textAlignment = View.TEXT_ALIGNMENT_TEXT_START
+    }
+
+    private fun showClearButton() {
+        setButtonDrawables(endOfTheText = clearBtnImage)
+    }
+
+    private fun setButtonDrawables(
+        startOfTheText: Drawable? = null,
+        topOfTheText: Drawable? = null,
+        endOfTheText: Drawable? = null,
+        bottomOfTheText: Drawable? = null,
+    ) {
+        setCompoundDrawablesWithIntrinsicBounds(startOfTheText,
+            topOfTheText,
+            endOfTheText,
+            bottomOfTheText)
+    }
+
+    private fun init() {
+        clearBtnImage = ContextCompat.getDrawable(context, R.drawable.ic_close) as Drawable
+        setOnTouchListener(this)
+        addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+                // do nothing
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+                if (s.toString().isNotEmpty()) showClearButton() else hideClearButton()
+            }
+
+            override fun afterTextChanged(s: Editable?) {
+                // do nothing
+            }
+        })
+    }
+
+    private fun hideClearButton() {
+        setButtonDrawables()
+    }
+
+    override fun onTouch(v: View?, event: MotionEvent?): Boolean {
+        if (compoundDrawables[2] != null) {
+            val clearButtonStart: Float
+            val clearButtonEnd: Float
+            var isClearButtonClicked = false
+            if (layoutDirection == View.LAYOUT_DIRECTION_RTL) {
+                clearButtonEnd = (clearBtnImage.intrinsicWidth + paddingStart).toFloat()
+                when {
+                    event!!.x < clearButtonEnd -> isClearButtonClicked = true
+                }
+            } else {
+                clearButtonStart = (width - paddingEnd - clearBtnImage.intrinsicWidth).toFloat()
+                when {
+                    event!!.x > clearButtonStart -> isClearButtonClicked = true
+                }
+            }
+            if (isClearButtonClicked) {
+                when (event?.action) {
+                    MotionEvent.ACTION_DOWN -> {
+                        clearBtnImage =
+                            ContextCompat.getDrawable(context, R.drawable.ic_close) as Drawable
+                        showClearButton()
+                        return true
+                    }
+                    MotionEvent.ACTION_UP -> {
+                        clearBtnImage =
+                            ContextCompat.getDrawable(context, R.drawable.ic_close) as Drawable
+                        when {
+                            text != null -> text?.clear()
+                        }
+                        hideClearButton()
+                        return true
+                    }
+                    else -> return false
+                }
+            } else return false
+        }
+        return false
+    }
+}
